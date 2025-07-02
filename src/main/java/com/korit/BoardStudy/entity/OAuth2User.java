4f91eed78753107de06440d1fd4803d710770736
Song-Da <songowod@naver.com>
2025-07-02 21:40:57 +0900

    init: first study

---
+++ src/main/java/com/korit/BoardStudy/entity/OAuth2User.java
@@ -0,0 +1,18 @@
+package com.korit.BoardStudy.entity;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class OAuth2User {
+    private Integer oAuth2UserId;
+    private Integer userId;
+    private String provider;
+    private String providerUserId;
+    private LocalDateTime createDt;
+    private LocalDateTime updateDt;
+
+}
